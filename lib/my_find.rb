a497576ec88827394ef38c83414f21f7e59af351
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-26 13:54:45 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -8,10 +8,12 @@ def my_find(collection)
  i = 0 
  
  while collection.length > i 
- if yield(collection[i])
-   return collection[i]
+  if yield(collection[i])
+    return collection[i]
+  end 
+  i = i + 1
  end 
- i = i + 1
+ return false
  
 end
 
